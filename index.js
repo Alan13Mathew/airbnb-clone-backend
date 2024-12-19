138a1fcdca4bc3b4a5fef5e190943467eb93a583
Alan Mathew <alanmathew0891@gmail.com>
2024-12-19 10:49:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const authRoutes  = require("./Routes/auth.routes")
+require("dotenv").config();
+
+const app = express();
+
+
+
+
+//middleware
+app.use(express.json());
+app.use(cors(
+    {
+        origin: process.env.CLIENT_URL,
+        credentials: true,
+    }
+));
+
+
+//routes
+
+app.use("/api/auth",authRoutes );
+
+//Database connection
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+  console.log("Connected to MongoDB");
+}).catch((err) => {
+  console.log('MongoDB connection error:', err);
+});
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
